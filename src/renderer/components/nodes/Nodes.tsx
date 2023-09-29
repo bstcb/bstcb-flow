6ca3fbbec3de2bcbef0164cb090e0426c9885359
cofeek-codes <kormyshev11@mail.ru>
2023-09-29 17:02:48 +0500

    flow setup

--- src/renderer/components/nodes/Nodes.tsx
+++ src/renderer/components/nodes/Nodes.tsx
@@ -1,9 +1,20 @@
+import { ReactFlow } from 'reactflow';
 import './Nodes.scss';
 
 const Nodes = () => {
-  return (
+
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
+
+    return (
     <div className="nodes">
-      <div className="nodes__wrapper">Nodes</div>
+	  <div className="nodes__wrapper">
+	    <ReactFlow nodes={initialNodes} edges={initialEdges}/>
+	  </div>
     </div>
   );
 };
