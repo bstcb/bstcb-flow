0e82e857bb297dcf0ca5f25799de29eb5b6c618d
cofeek-codes <kormyshev11@mail.ru>
2023-09-29 22:45:12 +0500

    nodes fixed

--- src/renderer/components/nodes/Nodes.tsx
+++ src/renderer/components/nodes/Nodes.tsx
@@ -1,20 +1,20 @@
 import { ReactFlow } from 'reactflow';
 import './Nodes.scss';
 
+import 'reactflow/dist/style.css';
+
 const Nodes = () => {
+  const initialNodes = [
+    { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+    { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+  ];
+  const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 
-const initialNodes = [
-  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
-  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
-];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
-
-
-    return (
+  return (
     <div className="nodes">
-	  <div className="nodes__wrapper">
-	    <ReactFlow nodes={initialNodes} edges={initialEdges}/>
-	  </div>
+      <div className="nodes__wrapper">
+        <ReactFlow nodes={initialNodes} edges={initialEdges} />
+      </div>
     </div>
   );
 };
