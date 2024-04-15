a305fe531345481057e2e6b59e9e16b91410ff6d
cofeek-codes <kormyshev11@mail.ru>
2024-04-15 17:38:50 +0500

    toasts are now animated

--- src/renderer/components/toasts/ErrorToast.tsx
+++ src/renderer/components/toasts/ErrorToast.tsx
@@ -1,10 +1,17 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { useStyleStore } from "../../store/StyleStore"
 
+// TODO: make toast universal
 const ErrorToast = (props: { message: string, isTriggered: boolean }) => {
-   useEffect(() => {
-
-   }, [props.isTriggered])
-   return <div className="alert alert-danger toast__alert" role="alert">
+   const [classes, setClasses] = useState(["alert", "alert-danger", "toast__alert"])
+   useStyleStore.subscribe((state) => {
+      setClasses(c => [...c, "active"])
+      const timer = setTimeout(() => {
+         setClasses(c => c.filter(e => e !== 'active'))
+         console.log('timeout')
+      }, 2000)
+   })
+   return <div className={classes.join(' ')} role="alert">
       {props.message}
    </div>
 
