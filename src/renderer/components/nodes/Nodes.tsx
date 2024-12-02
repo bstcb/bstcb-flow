c95bd6ac9da4b343c567f88fff70d35ae71ff218
cofeek-codes <kormyshev11@mail.ru>
2024-12-02 16:25:22 +0500

    [Nodes.tsx]: reduced new node spawn position distance

--- src/renderer/components/nodes/Nodes.tsx
+++ src/renderer/components/nodes/Nodes.tsx
@@ -1,12 +1,12 @@
 import {
-   Controls,
-   Edge,
-   Node,
-   OnConnect,
-   ReactFlow,
-   addEdge,
-   useEdgesState,
-   useNodesState,
+	Controls,
+	Edge,
+	Node,
+	OnConnect,
+	ReactFlow,
+	addEdge,
+	useEdgesState,
+	useNodesState,
 } from 'reactflow'
 import { v4 as uuid } from 'uuid'
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
@@ -22,115 +22,116 @@ import ContextMenu from '../ContextMenu'
 import './Nodes.scss'
 
 const Nodes = () => {
-   // variables
-   const [nodes, setNodes, onNodesChange] = useNodesState<Node[]>(initialNodes)
-   const [edges, setEdges, onEdgesChange] = useEdgesState<Edge[]>(initialEdges)
-   const [menu, setMenu] = useState(null);
-   const edgeUpdateSuccessful = useRef(true);
-   const ref = useRef(null);
-   // functions for edge delition
-   const onEdgeUpdateStart = useCallback(() => {
-      edgeUpdateSuccessful.current = false;
-   }, []);
+	// variables
+	const [nodes, setNodes, onNodesChange] = useNodesState<Node[]>(initialNodes)
+	const [edges, setEdges, onEdgesChange] = useEdgesState<Edge[]>(initialEdges)
+	const [menu, setMenu] = useState(null)
+	const edgeUpdateSuccessful = useRef(true)
+	const ref = useRef(null)
+	// functions for edge delition
+	const onEdgeUpdateStart = useCallback(() => {
+		edgeUpdateSuccessful.current = false
+	}, [])
 
-   const onEdgeUpdate = useCallback((oldEdge, newConnection) => {
-      edgeUpdateSuccessful.current = true;
-      setEdges((els) => updateEdge(oldEdge, newConnection, els));
-   }, []);
+	const onEdgeUpdate = useCallback((oldEdge, newConnection) => {
+		edgeUpdateSuccessful.current = true
+		setEdges(els => updateEdge(oldEdge, newConnection, els))
+	}, [])
 
-   const onEdgeUpdateEnd = useCallback((_, edge) => {
-      if (!edgeUpdateSuccessful.current) {
-         setEdges((eds) => eds.filter((e) => e.id !== edge.id));
-      }
+	const onEdgeUpdateEnd = useCallback((_, edge) => {
+		if (!edgeUpdateSuccessful.current) {
+			setEdges(eds => eds.filter(e => e.id !== edge.id))
+		}
 
-      edgeUpdateSuccessful.current = true;
-   }, []);
+		edgeUpdateSuccessful.current = true
+	}, [])
 
-   useEffect(() => {
-      window.electron.ipcRenderer.on(
-         'add-node',
-         (nodeType: string, value: string) => {
-            let newNode: Node = {
-               id: `_${nodeType}_${uuid()}`,
-               type: nodeType,
-               position: {
-                  x: getRandomInt(300, 500),
-                  y: getRandomInt(300, 500),
-               },
-               data: { label: value, value },
-            }
-            setNodes(nds => nds.concat(newNode))
-         },
-      )
-      return () => {}
-   }, [])
+	useEffect(() => {
+		window.electron.ipcRenderer.on(
+			'add-node',
+			(nodeType: string, value: string) => {
+				let newNode: Node = {
+					id: `_${nodeType}_${uuid()}`,
+					type: nodeType,
+					position: {
+						x: getRandomInt(100, 100),
+						y: getRandomInt(100, 300),
+					},
+					data: { label: value, value },
+				}
+				setNodes(nds => nds.concat(newNode))
+			},
+		)
+		return () => {}
+	}, [])
 
-   const onConnect: OnConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
+	const onConnect: OnConnect = useCallback(
+		params => setEdges(eds => addEdge(params, eds)),
+		[setEdges],
+	)
 
-   const onNodeContextMenu = useCallback(
-      (event, node) => {
-         // Prevent native context menu from showing
-         event.preventDefault();
-         console.log(event)
-         // Calculate position of the context menu. We want to make sure it
-         // doesn't get positioned off-screen.
-         const pane = ref.current.getBoundingClientRect();
-         console.log(pane)
-         setMenu({
-            id: node.id,
-            top: event.clientY < pane.height - 200 && event.clientY,
-            left: event.clientX < pane.width - 200 && event.clientX,
-            right: event.clientX >= pane.width - 200 && pane.width - event.clientX,
-            bottom:
-               event.clientY >= pane.height - 200 && pane.height - event.clientY,
-         });
-         console.log(menu)
-      },
-      [setMenu],
-   );
+	const onNodeContextMenu = useCallback(
+		(event, node) => {
+			// Prevent native context menu from showing
+			event.preventDefault()
+			console.log(event)
+			// Calculate position of the context menu. We want to make sure it
+			// doesn't get positioned off-screen.
+			const pane = ref.current.getBoundingClientRect()
+			console.log(pane)
+			setMenu({
+				id: node.id,
+				top: event.clientY < pane.height - 200 && event.clientY,
+				left: event.clientX < pane.width - 200 && event.clientX,
+				right: event.clientX >= pane.width - 200 && pane.width - event.clientX,
+				bottom:
+					event.clientY >= pane.height - 200 && pane.height - event.clientY,
+			})
+			console.log(menu)
+		},
+		[setMenu],
+	)
 
-   const onPaneClick = useCallback(() => setMenu(null), [setMenu]);
+	const onPaneClick = useCallback(() => setMenu(null), [setMenu])
 
-   const nodeTypes = useMemo(
-      () => ({
-         _input: InputNode,
-         _output: OutputNode,
-         _if_cond: IfConditionNode,
-         _for_lp: ForLoopNode,
-         _while_lp: WhileLoopNode,
-      }),
-      [],
-   )
+	const nodeTypes = useMemo(
+		() => ({
+			_input: InputNode,
+			_output: OutputNode,
+			_if_cond: IfConditionNode,
+			_for_lp: ForLoopNode,
+			_while_lp: WhileLoopNode,
+		}),
+		[],
+	)
 
-
-
-   return (
-      <div className='nodes'>
-         <div className='nodes__wrapper'>
-            <ReactFlow
-               ref={ref}
-               nodes={nodes}
-               edges={edges}
-               onConnect={onConnect}
-               onNodesChange={onNodesChange}
-               onEdgesChange={onEdgesChange}
-               nodeTypes={nodeTypes}
-               onConnect={onConnect}
-               // content menu
-               onPaneClick={onPaneClick}
-               onNodeContextMenu={onNodeContextMenu}
-               fitView
-               // node update props
-               onEdgeUpdate={onEdgeUpdate}
-               onEdgeUpdateStart={onEdgeUpdateStart}
-               onEdgeUpdateEnd={onEdgeUpdateEnd}
-            >
-               <Controls />
-               {menu && <ContextMenu onClick={onPaneClick} {...menu} />}
-            </ReactFlow>
-         </div>
-      </div>
-   )
+	return (
+		<div className='nodes'>
+			<div className='nodes__wrapper'>
+				<ReactFlow
+					ref={ref}
+					nodes={nodes}
+					edges={edges}
+					onConnect={onConnect}
+					onNodesChange={onNodesChange}
+					onEdgesChange={onEdgesChange}
+					nodeTypes={nodeTypes}
+					onConnect={onConnect}
+					// content menu
+					onPaneClick={onPaneClick}
+					onNodeContextMenu={onNodeContextMenu}
+					fitView
+					// node update props
+					onEdgeUpdate={onEdgeUpdate}
+					onEdgeUpdateStart={onEdgeUpdateStart}
+					onEdgeUpdateEnd={onEdgeUpdateEnd}
+				>
+					<Controls />
+					{menu && <ContextMenu onClick={onPaneClick} {...menu} />}
+				</ReactFlow>
+			</div>
+		</div>
+	)
 }
 
 export default Nodes
