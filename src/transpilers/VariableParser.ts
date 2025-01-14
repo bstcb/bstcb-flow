8dad4124eb0de5cfe80226c2704ac2aa3f3fcd41
cofeek-codes <kormyshev11@mail.ru>
2025-01-14 17:48:37 +0500

    `NTK_OUTPUT` case implemented in VariableParser

--- src/transpilers/VariableParser.ts
+++ src/transpilers/VariableParser.ts
@@ -2,33 +2,36 @@ import { Variable } from '../renderer/types/variable'
 import { NodeTokenKind } from './Token'
 
 export abstract class VariableParser {
-	static parse(value: string, kind: NodeTokenKind): Variable {
-		// @TODO: fix this workaround. Sometimes value comes as object
-		if (typeof value === 'string') {
-			switch (kind) {
-				case NodeTokenKind.NTK_IF_CONDITION:
-					throw new Error(`${kind} variable parser is not implemented`)
-					return {} as Variable
-				case NodeTokenKind.NTK_FOR_LOOP:
-					throw new Error(`${kind} variable parser is not implemented`)
-					return {} as Variable
-				case NodeTokenKind.NTK_WHILE_LOOP:
-					throw new Error(`${kind} variable parser is not implemented`)
-					return {} as Variable
-				case NodeTokenKind.NTK_INPUT:
-					return {
-						name: value.split('=')[0].trim(),
-						value: value.split('=')[1].trim(),
-					}
-				case NodeTokenKind.NTK_OUTPUT:
-					throw new Error(`${kind} variable parser is not implemented`)
-					return {} as Variable
-				case NodeTokenKind.NTK_INITAL:
-					throw new Error(`${kind} variable parser is not implemented`)
-					return {} as Variable
-			}
-		} else {
-			return value
-		}
-	}
+    static parse(value: string, kind: NodeTokenKind): Variable {
+        // @TODO: fix this workaround. Sometimes value comes as object
+        if (typeof value === 'string') {
+            switch (kind) {
+                // @TODO: handle potential wrong input type error
+                case NodeTokenKind.NTK_IF_CONDITION:
+                    throw new Error(`${kind} variable parser is not implemented`)
+                    return {} as Variable
+                case NodeTokenKind.NTK_FOR_LOOP:
+                    throw new Error(`${kind} variable parser is not implemented`)
+                    return {} as Variable
+                case NodeTokenKind.NTK_WHILE_LOOP:
+                    throw new Error(`${kind} variable parser is not implemented`)
+                    return {} as Variable
+                case NodeTokenKind.NTK_INPUT:
+                    return {
+                        name: value.split('=')[0].trim(),
+                        value: value.split('=')[1].trim(),
+                    }
+                case NodeTokenKind.NTK_OUTPUT:
+                    return {
+                        name: value
+                        value: value.split(',').trim()
+                    }
+                case NodeTokenKind.NTK_INITAL:
+                    throw new Error(`${kind} variable parser is not implemented`)
+                    return {} as Variable
+            }
+        } else {
+            return value
+        }
+    }
 }
