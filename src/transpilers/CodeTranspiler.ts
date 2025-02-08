5dced4f7f4851e8c43318775b9b0e78e05e9e564
cofeek-codes <kormyshev11@mail.ru>
2025-02-08 10:03:28 +0500

    `parse-code` ipc channels and functions

--- src/transpilers/CodeTranspiler.ts
+++ src/transpilers/CodeTranspiler.ts
@@ -3,6 +3,14 @@ import { NodeGen } from './NodeGen'
 import { VariableParser } from './VariableParser'
 import { NodeTokenKind } from './Token'
 import { Variable } from '../renderer/types/variable'
+import { ipcRenderer } from 'electron'
+import { CodeLanguage } from './CodeLanguage'
+import { useCodeStore } from '../renderer/store/CodeStore'
+
+export type ParsableCode = {
+    language: CodeLanguage,
+    codeChunks: string[]
+}
 
 export class CodeTranspiler {
     rfInstance: ReactFlowInstance
@@ -21,6 +29,21 @@ export class CodeTranspiler {
         // NodeGen.genOutput({ name: 'a,b', value: 'a,b' }, 4, this.rfInstance)
         // NodeGen.genFor({ name: 'let i = 0; i < 5; i++', value: 'let i = 0; i < 5; i++' }, 1, this.rfInstance)
         // NodeGen.genWhile({ name: 'i < 5', value: 'i < 5' }, 1, this.rfInstance)
+
+        let codeStoreState = useCodeStore.getState()
+
+        let parsableCode: ParsableCode = {
+            language: codeStoreState.activeLanguage,
+            codeChunks: codeStoreState.codeChunks
+        }
+
+        window.electron.ipcRenderer.sendMessage('parse-code', parsableCode)
+
+        window.electron.ipcRenderer.on('parse-code', async (result) => {
+            console.log('parsed code returned')
+            console.log('result')
+            console.log(result)
+        })
     }
 }
 
