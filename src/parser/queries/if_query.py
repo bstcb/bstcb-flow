63259ae1604e628bb3337577cea1a552e5837398
cofeek-codes <kormyshev11@mail.ru>
2025-02-12 11:58:53 +0500

    [Parser]: if query done

---
+++ src/parser/queries/if_query.py
@@ -0,0 +1,19 @@
+from tree_sitter import Language
+
+
+# (call_expression function: (identifier)
+#  arguments: (arguments ( (identifier) )))
+
+
+def make_if_node(lang: Language, node):
+    # get logging fiunctions names only?
+    query = lang.query(
+        """
+        (if_statement
+        condition: (_) @cond
+        )
+        """
+    )
+
+    print(query.matches(node)[0][1]['cond']
+          [0].text.decode('utf8').strip('()'))
