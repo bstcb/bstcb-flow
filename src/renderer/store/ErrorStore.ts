fc8fb682d40949e9de4a854d9455759e40dc49a7
cofeek-codes <kormyshev11@mail.ru>
2025-02-24 14:19:35 +0500

    [ErrorStore]: modified to use `subscribeWithSelector`

--- src/renderer/store/ErrorStore.ts
+++ src/renderer/store/ErrorStore.ts
@@ -1,36 +1,45 @@
-import { create } from 'zustand'
+import { createStore } from 'zustand/vanilla'
+import { subscribeWithSelector } from 'zustand/middleware'
 
 type State = {
-  // general field to check error existence
-  isError: boolean
-  // data format error
-  isNodeDataFormatError: string
-  setIsNodeDataFormatError: (ne: string) => void
-  // attempt to delete initial node error
-  isDeleteNodeError: boolean
-  setIsDeleteNodeError: (ne: boolean) => void
-  // reset state
-  clearErrors: () => void
+    // general field to check error existence
+    isError: boolean
+    // data format error
+    isNodeDataFormatError: string
 }
 
-export const useErrorStore = create<State>(set => ({
-  isDeleteNodeError: false,
-  isNodeDataFormatError: '',
-  isError: false,
-  setIsDeleteNodeError: (ne: boolean) => {
-    set(() => ({ isDeleteNodeError: ne, isError: true }))
-    console.log('[STORE]: node error toast updated: ', ne)
-  },
-  setIsNodeDataFormatError: (ne: string) => {
-    set(() => ({ isNodeDataFormatError: ne, isError: true }))
-    console.log('[STORE]: node error toast updated: ', ne)
-  },
-  clearErrors: () => {
-    set(() => ({
-      isNodeDataFormatError: '',
-      isError: false,
-      isDeleteNodeError: false,
+type Actions = {
+    setIsNodeDataFormatError: (ne: string) => void
+    // attempt to delete initial node error
+    isDeleteNodeError: boolean
+    setIsDeleteNodeError: (ne: boolean) => void
+    // reset state
+    clearErrors: () => void
+}
+
+type Store = State & Actions
+
+
+export const useErrorStore = createStore<Store>()(
+    subscribeWithSelector((set) => ({
+        isDeleteNodeError: false,
+        isNodeDataFormatError: '',
+        isError: false,
+        setIsDeleteNodeError: (ne: boolean) => {
+            set(() => ({ isDeleteNodeError: ne, isError: true }))
+            console.log('[STORE]: node error toast updated: ', ne)
+        },
+        setIsNodeDataFormatError: (ne: string) => {
+            set(() => ({ isNodeDataFormatError: ne, isError: true }))
+            console.log('[STORE]: node error toast updated: ', ne)
+        },
+        clearErrors: () => {
+            set(() => ({
+                isNodeDataFormatError: '',
+                isError: false,
+                isDeleteNodeError: false,
+            }))
+            console.log('[STORE]: node error store cleared:')
+        },
     }))
-    console.log('[STORE]: node error store cleared: ', useErrorStore.getState())
-  },
-}))
+)
