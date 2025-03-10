7d2c5ebee55230c41657dec7ab014b2a2851733b
cofeek-codes <kormyshev11@mail.ru>
2025-03-10 16:24:02 +0500

    [preferences]: manual updates

--- src/renderer/components/preferences/Preferences.tsx
+++ src/renderer/components/preferences/Preferences.tsx
@@ -2,21 +2,33 @@ import { SettingsPane, SettingsPage, SettingsContent, SettingsMenu } from 'react
 
 import './Preferences.scss'
 import { preferencesMenu } from './preferencesMenu';
-import { preferences } from './preferences';
+import { Preferences, preferences } from './preferences';
+import { usePreferencesStore } from '../../store/PreferencesStore';
+import { useState } from 'react';
 
 const Preferences = () => {
+    const savedPrefs = usePreferencesStore.getState().preferences
+    const [newPrefs, setNewPrefs] = useState<Preferences>(savedPrefs)
 
     // Save settings after close
-    const leavePaneHandler = (wasSaved, newSettings, oldSettings) => {
+    const leavePaneHandler = (_wasSaved, _newSettings, _oldSettings) => {
         // "wasSaved" indicates wheather the pane was just closed or the save button was clicked.
 
-        if (wasSaved && newSettings !== oldSettings) {
-            // do something with the settings, e.g. save via ajax.
-        }
     };
 
-    const settingsChanged = (changedSettings) => {
-        // this is triggered onChange of the inputs
+    const settingsChanged = (e) => {
+        // debugger
+        let changedPrefSelector: HTMLElement = e.target
+        let changedPrefName: string = changedPrefSelector.name
+        let changedPrefValue: string = changedPrefSelector.value // not only string
+        let prevPrefValue: string = newPrefs[changedPrefName]
+        // show that setting is changed in ui
+        if (prevPrefValue !== changedPrefValue) {
+            changedPrefSelector.classList.add('settings-changed')
+        } else {
+            changedPrefSelector.classList.remove('settings-changed')
+
+        }
     };
 
     return <>
@@ -26,7 +38,7 @@ const Preferences = () => {
                 <SettingsPage handler={preferencesMenu[0].url}>
                     <fieldset className="form-group">
                         <label htmlFor="colorTheme">Color-Theme: </label>
-                        <select name={Object.keys(preferences)[0]} id="colorTheme" className="form-control" defaultValue={preferences['preferences.appearance.colorTheme']}>
+                        <select name={Object.keys(preferences)[0]} onChange={settingsChanged} id="colorTheme" className="form-control" defaultValue={preferences['preferences.appearance.colorTheme']}>
                             <option value={'light'}>Light</option>
                             <option value={'dark'}>Dark</option>
                         </select>
