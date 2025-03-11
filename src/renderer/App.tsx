af5269dd71976662dcac0288c8a2f67198451e28
cofeek-codes <kormyshev11@mail.ru>
2025-03-12 01:56:37 +0500

    [theming]: implement proper theme change

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -4,17 +4,27 @@ import Nodes from './components/nodes/Nodes'
 import { useEffect, useRef, useState } from 'react'
 import { ToastContainer } from 'react-toastify'
 import DockLayout, { DropDirection, FloatPosition, LayoutBase } from 'rc-dock'
-// light theme and custom file must go together, because dark theme remains unchanged 
-// @TODO: dynamically choose (import) theme
-// import 'rc-dock/dist/rc-dock-dark.css';
 import { useLayoutStore } from './store/LayoutStore'
 import { usePreferencesStore } from './store/PreferencesStore'
 import { setupMenuEventHandler } from './menuHandler'
-import './App.scss'
-import 'rc-dock/dist/rc-dock.css';
-import './rc-dock-custom.scss'
 import { preferencesPanel } from './docks/preferencesPanel'
 import { defaultLayout } from './docks/defaultLayout'
+import { Preferences } from './components/preferences/preferences'
+
+import './App.scss'
+
+// import 'rc-dock/dist/rc-dock-dark.css';
+import 'rc-dock/dist/rc-dock.css';
+import './rc-dock-custom.scss'
+
+
+// @TODO: implement
+function changeTheme(theme: Pick<Preferences, 'preferences.appearance.colorTheme'>) {
+    if (theme == 'dark') {
+    } else {
+
+    }
+}
 
 const App = () => {
     const dockLayoutRef = useRef(null)
@@ -37,7 +47,12 @@ const App = () => {
             }
         })
         // preferences store listener
+        // @TODO: optimize
+        // we don't need to watch all preferences here
         usePreferencesStore.subscribe(state => {
+            // watch theme
+            changeTheme(state.preferences['preferences.appearance.colorTheme'])
+
             const isPreferencesTabDisplayed = dock.find('preferences')
             // display preferences
             if (state.isPreferencesOpened && !isPreferencesTabDisplayed) {
