0da048e8842b34c4cfe29bfc88dfab183dbd9563
cofeek-codes <kormyshev11@mail.ru>
2025-03-15 17:49:06 +0500

    [NodeTranspiler]: generating block end nodes

--- src/transpilers/Codegen.ts
+++ src/transpilers/Codegen.ts
@@ -4,74 +4,85 @@ import { Variable } from '../renderer/types/variable'
 import { CodeLanguage } from './CodeLanguage'
 
 function applyChunk(chunk: string) {
-  useCodeStore
-    .getState()
-    .setCodeChunks([...useCodeStore.getState().codeChunks, chunk])
+    useCodeStore
+        .getState()
+        .setCodeChunks([...useCodeStore.getState().codeChunks, chunk])
 }
 
 export class CodeGen {
-  static activeLanguage: CodeLanguage
-  static getActiveLang() {
-    this.activeLanguage = enumFromString(
-      CodeLanguage,
-      useCodeStore.getState().activeLanguage,
-    )!
-  }
-
-  static genInput(variable: Variable) {
-    switch (this.activeLanguage) {
-      case CodeLanguage.LANG_JS:
-        console.log(
-          `input generation in ${this.activeLanguage} with variable: ${variable.name}: ${variable.value}`,
-        )
-        const chunk = `let ${variable.name} = ${variable.value}\n`
-        applyChunk(chunk)
-        break
+    static activeLanguage: CodeLanguage
+    static getActiveLang() {
+        this.activeLanguage = enumFromString(
+            CodeLanguage,
+            useCodeStore.getState().activeLanguage,
+        )!
     }
-  }
 
-  static genOutput(expr: string) {
-    switch (this.activeLanguage) {
-      case CodeLanguage.LANG_JS:
-        console.log(
-          `output generation in ${this.activeLanguage} with variable: ${expr}`,
-        )
-        const chunk = `console.log(${expr})\n`
-        applyChunk(chunk)
-        break
+    static genInput(variable: Variable) {
+        switch (this.activeLanguage) {
+            case CodeLanguage.LANG_JS:
+                console.log(
+                    `input generation in ${this.activeLanguage} with variable: ${variable.name}: ${variable.value}`,
+                )
+                const chunk = `let ${variable.name} = ${variable.value}\n`
+                applyChunk(chunk)
+                break
+        }
     }
-  }
 
-  static genIf(expr: string) {
-    switch (this.activeLanguage) {
-      case CodeLanguage.LANG_JS:
-        console.log(
-          `if condition generation in ${this.activeLanguage} with expression: ${expr}`,
-        )
-        const chunk = `if (${expr}) {\n`
-        applyChunk(chunk)
+    static genOutput(expr: string) {
+        switch (this.activeLanguage) {
+            case CodeLanguage.LANG_JS:
+                console.log(
+                    `output generation in ${this.activeLanguage} with variable: ${expr}`,
+                )
+                const chunk = `console.log(${expr})\n`
+                applyChunk(chunk)
+                break
+        }
     }
-  }
 
-  static genWhile(expr: string) {
-    switch (this.activeLanguage) {
-      case CodeLanguage.LANG_JS:
-        console.log(
-          `while loop generation in ${this.activeLanguage} with expression: ${expr}`,
-        )
-        const chunk = `while (${expr}) {\n`
-        applyChunk(chunk)
+    static genIf(expr: string) {
+        switch (this.activeLanguage) {
+            case CodeLanguage.LANG_JS:
+                console.log(
+                    `if condition generation in ${this.activeLanguage} with expression: ${expr}`,
+                )
+                const chunk = `if (${expr}) {\n`
+                applyChunk(chunk)
+        }
     }
-  }
 
-  static genFor(expr: string) {
-    switch (this.activeLanguage) {
-      case CodeLanguage.LANG_JS:
-        console.log(
-          `for loop generation in ${this.activeLanguage} with expression: ${expr}`,
-        )
-        const chunk = `for (${expr}) {\n`
-        applyChunk(chunk)
+    static genWhile(expr: string) {
+        switch (this.activeLanguage) {
+            case CodeLanguage.LANG_JS:
+                console.log(
+                    `while loop generation in ${this.activeLanguage} with expression: ${expr}`,
+                )
+                const chunk = `while (${expr}) {\n`
+                applyChunk(chunk)
+        }
     }
-  }
+
+    static genFor(expr: string) {
+        switch (this.activeLanguage) {
+            case CodeLanguage.LANG_JS:
+                console.log(
+                    `for loop generation in ${this.activeLanguage} with expression: ${expr}`,
+                )
+                const chunk = `for (${expr}) {\n`
+                applyChunk(chunk)
+        }
+    }
+
+    static genBlockEnd() {
+        switch (this.activeLanguage) {
+            case CodeLanguage.LANG_JS:
+                console.log(
+                    `block end generation in ${this.activeLanguage}`,
+                )
+                const chunk = `}\n`
+                applyChunk(chunk)
+        }
+    }
 }
