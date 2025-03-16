6e82f569712304e6d8d95fe5c9a892f7d16eaebb
cofeek-codes <kormyshev11@mail.ru>
2025-03-16 15:24:39 +0500

    [ErrorReporter]: `ShowUnbalancedBlock` function added

--- src/renderer/errors/ErrorReporter.ts
+++ src/renderer/errors/ErrorReporter.ts
@@ -4,48 +4,64 @@ import { useErrorStore } from "../store/ErrorStore"
 import { toast, Bounce } from "react-toastify";
 
 export enum ErrorKind {
-    EK_WRONG_DATA_FORMAT = "wrong data format"
+  EK_WRONG_DATA_FORMAT = "wrong data format"
 }
 
 export class ErrorReporter {
-    // Typescript dosen't have proper overloads :(
-    static showShort(errorMessage: string) {
-        toast.error(errorMessage, {
-            position: "bottom-right",
-            autoClose: TIMER_DURATION,
-            hideProgressBar: true,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "colored",
-            transition: Bounce,
-        });
-    }
+  // Typescript dosen't have proper overloads :(
+  static showShort(errorMessage: string) {
+    toast.error(errorMessage, {
+      position: "bottom-right",
+      autoClose: TIMER_DURATION,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+      transition: Bounce,
+    });
+  }
 
-    static show(nodeType: string, nodeIndex: number, errorKind: ErrorKind, errorMessage: string) {
-        const errorString = `${errorMessage} in ${nodeType.replace('_', '')} node at position ${nodeIndex}`
-        toast.error(errorString, {
-            position: "bottom-right",
-            autoClose: TIMER_DURATION,
-            hideProgressBar: true,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "colored",
-            transition: Bounce,
-        });
+  static show(nodeType: string, nodeIndex: number, errorKind: ErrorKind, errorMessage: string) {
+    const errorString = `${errorMessage} in ${nodeType.replace('_', '')} node at position ${nodeIndex}`
+    toast.error(errorString, {
+      position: "bottom-right",
+      autoClose: TIMER_DURATION,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+      transition: Bounce,
+    });
 
-    }
+  }
 
-    static applyErrorStyle(ref: MutableRefObject<HTMLInputElement>) {
-        if (!ref.current.className.includes(NODE_ERROR_CLASSNAME))
-            ref.current.className = NODE_ERROR_CLASSNAME
-    }
+  static showUnbalancedBlock(missingPart: 'start' | 'end', nodeType: NodeTokenKind, nodeIndex: number) {
+    const errorString = `unbalacned block: ${nodeType.replace('_', '')} at position ${nodeIndex} missing corresponding ${missingPart} block`
+    toast.error(errorString, {
+      position: "bottom-right",
+      autoClose: TIMER_DURATION,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+      transition: Bounce,
+    });
 
-    static clearErrorStyle(ref: MutableRefObject<HTMLInputElement>) {
-        if (ref.current.className.includes(NODE_ERROR_CLASSNAME))
-            ref.current.className = ''
-    }
+  }
+
+  static applyErrorStyle(ref: MutableRefObject<HTMLInputElement>) {
+    if (!ref.current.className.includes(NODE_ERROR_CLASSNAME))
+      ref.current.className = NODE_ERROR_CLASSNAME
+  }
+
+  static clearErrorStyle(ref: MutableRefObject<HTMLInputElement>) {
+    if (ref.current.className.includes(NODE_ERROR_CLASSNAME))
+      ref.current.className = ''
+  }
 }
