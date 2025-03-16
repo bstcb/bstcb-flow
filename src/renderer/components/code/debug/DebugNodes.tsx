80f7c337868cb994aa9d365e59f886a78cb50cff
cofeek-codes <kormyshev11@mail.ru>
2025-03-16 17:33:44 +0500

    [DebugNodes]: start part of block validation is done

--- src/renderer/components/code/debug/DebugNodes.tsx
+++ src/renderer/components/code/debug/DebugNodes.tsx
@@ -2,6 +2,8 @@ import { Node, Edge, getConnectedEdges, useReactFlow } from 'reactflow'
 import { useCodeStore } from '../../../store/CodeStore'
 import { NodeTranspiler } from '../../../../transpilers/NodeTranspiler'
 import { ErrorReporter } from '../../../errors/ErrorReporter'
+import { NodeTokenKind } from '../../../../transpilers/Token'
+import { NODE_ERROR_CLASSNAME } from '../../../constants'
 
 const DebugNodes = () => {
     const { getNodes, getEdges, getNode } = useReactFlow()
@@ -46,6 +48,41 @@ const DebugNodes = () => {
 
         console.log('[DEBUG]: active nodes are:')
         console.log(activeNodes)
+
+        // validate blocks
+        let blockTypePairs = {
+            [NodeTokenKind.NTK_IF_CONDITION]: NodeTokenKind.NTK_IF_CONDITION_END,
+            [NodeTokenKind.NTK_FOR_LOOP]: NodeTokenKind.NTK_FOR_LOOP_END,
+            [NodeTokenKind.NTK_WHILE_LOOP]: NodeTokenKind.NTK_WHILE_LOOP_END,
+        }
+
+        let blockStartTypes = Object.keys(blockTypePairs)
+        let blockEndTypes = Object.values(blockTypePairs)
+
+        // clear previos possible error styles
+        document.querySelectorAll('.' + NODE_ERROR_CLASSNAME).forEach(ErrorReporter.clearErrorStyle)
+
+        for (let i = 0; i < activeNodes.length; i++) {
+            let n = activeNodes[i]
+            // blocks (start)
+            // debugger
+            if (blockStartTypes.includes(n.data.type)) {
+                let arrayTail = activeNodes.slice(activeNodes.indexOf(n) + 1)
+                let correspondingEnd = arrayTail.find(tn => tn.data.type == blockTypePairs[n.data.type])
+                if (!correspondingEnd) {
+                    // report error
+                    ErrorReporter.showUnbalancedBlock('end', n.data.type, activeNodes.indexOf(n))
+                    let nodeSelectorString = `div[data-id=${n.id}]>div.${n.data.type}_wrapper`
+                    let nodeSelector = document.querySelector(nodeSelectorString)
+                    ErrorReporter.applyErrorStyle(nodeSelector)
+                    break
+                }
+            }
+            // brackets (ends)
+            else if (blockEndTypes.includes(n.data.type)) {
+
+            }
+        }
 
         useCodeStore.getState().clearCodeChunks()
 
